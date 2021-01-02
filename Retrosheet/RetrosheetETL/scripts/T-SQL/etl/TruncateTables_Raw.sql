5ac446beb4467e67c47e9dc3474b6ba44b40966f
Jonathan Vlk <jonathan.vlk10@gmail.com>
2021-01-02 15:43:13 -0600

    added task to truncate unneeded raw data

---
+++ Retrosheet/RetrosheetETL/scripts/T-SQL/etl/TruncateTables_Raw.sql
@@ -0,0 +1,12 @@
+USE [Retrosheet]
+GO
+
+
+TRUNCATE TABLE [raw].[PlayerMaster]
+TRUNCATE TABLE [raw].[ParkMaster]
+TRUNCATE TABLE [raw].[FranchiseMaster]
+TRUNCATE TABLE [raw].[TeamMaster]
+TRUNCATE TABLE [raw].[Game]
+TRUNCATE TABLE [raw].[Event]
+GO
+
