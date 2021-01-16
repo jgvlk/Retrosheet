6985b99cf0160b5fe765d95fdba4633248cbbd6a
Jonathan Vlk <jonathan.vlk10@gmail.com>
2021-01-16 13:12:39 -0600

    added function to generate MLBGameIDs

---
+++ Retrosheet/RetrosheetDB/db/dbo/Programmability/Functions/dbo.udf_MLBGameIDConstructor.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [dbo].[udf_MLBGameIDConstructor]()
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT
+		[GameID]
+		,[RetroGameID]
+		,[Date]
+		,REPLACE([Date], '-', '_') + '_' + LOWER([VisitingTeam]) + 'mlb_' + LOWER([HomeTeam]) + 'mlb_' + ( CASE WHEN [GameNumber] IN (0, 1) THEN '1' ELSE CONVERT(VARCHAR(1), [GameNumber]) END ) AS [MLBGameID]
+	FROM
+		[dbo].[Game]
+)
+GO
+
