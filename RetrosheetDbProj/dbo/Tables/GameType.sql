e28fd1c3a6e1d00d46eeca82439e4ee3ec0d0f83
Jonathan Vlk <jvlk@westmonroe.com>
2023-09-21 15:27:48 -0500

    cleanup; implemented processing of event, allpost, and ngl data

--- RetrosheetDbProj/dbo/Tables/GameType.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[GameType] (
-    [GameTypeID] INT          NOT NULL,
-    [GameType]   VARCHAR (25) NOT NULL,
-    [dCreated]    DATETIME      CONSTRAINT [df_GameType_dCreated] DEFAULT (getdate()) NOT NULL,
-    [dModified]   DATETIME      CONSTRAINT [df_GameType_dModified] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [pk_GameType] PRIMARY KEY CLUSTERED ([GameTypeID] ASC)
-);
-
--- etlv2.py
+++ etlv2.py
@@ -28,7 +28,7 @@ class RetrosheetEtl:
         self.run_dir: Path = self.data_dir / "run"
 
         self.all_data_zip: Path = self.download_dir / "alldata.zip"
-        
+
         self.allas_dir: Path = self.extract_dir / "allas"
         self.allpost_dir: Path = self.extract_dir / "allpost"
         self.boxes_dir: Path = self.extract_dir / "boxes"
@@ -36,6 +36,7 @@ class RetrosheetEtl:
         self.events_dir: Path = self.extract_dir / "events"
         self.gamelogs_dir: Path = self.extract_dir / "gamelogs"
         self.ngldata_dir: Path = self.extract_dir / "ngldata"
+        self.ngldata_events_dir: Path = self.ngldata_dir / "ngl-events"
         self.rosters_dir: Path = self.extract_dir / "rosters"
         self.schedule_dir: Path = self.extract_dir / "schedule"
         self.teams_dir: Path = self.extract_dir / "teams1871-2022"
@@ -44,10 +45,18 @@ class RetrosheetEtl:
         self.ejections_file: Path = self.extract_dir / "ejections.csv"
         self.teams_file: Path = self.extract_dir / "teams.csv"
 
+        now = dt.now()
+        self.game_log_file: Path = self.log_dir / "game_proc_{}.log".format(
+            now.strftime(r"%Y%m%d%H%M%S")
+        )
+        self.event_log_file: Path = self.log_dir / "event_proc_{}.log".format(
+            now.strftime(r"%Y%m%d%H%M%S")
+        )
+
         self.game_output_dir: Path = self.output_dir / "game"
         self.event_output_dir: Path = self.output_dir / "event"
 
-        self.file_extensions = ["EDA", "EVA", ".EVE", ".EVF", ".EVN", ".EVR"]
+        self.file_extensions: list = [".EDA", ".EDN", ".EVA", ".EVE", ".EVF", ".EVN", ".EVR"]
 
         self.url_all_data: Path = "https://www.retrosheet.org/downloads/alldata.zip"
 
@@ -56,34 +65,63 @@ class RetrosheetEtl:
         with open(self.all_data_zip, "wb") as f:
             f.write(all_data.content)
         return None
-    
+
     def _mkdir(self, path: Path) -> None:
         if path.exists():
             pass
         else:
             os.mkdir(path)
         return None
-    
-    def _proc_event(self):
-        pass
-    
-    def _proc_game_event(self, file: Path):
+
+    def _proc_game_event(self, file: Path) -> None:
         file_name = file.name.replace(".", "_")
         file_year = file_name[0:4]
         game_file_dest = self.game_output_dir / f"game_{file_name}.csv"
         event_file_dest = self.event_output_dir / f"event_{file_name}.csv"
-        game_cmd = ["bgame", "-f", "0-85", "-y", str(file_year), "-s", "0101", "-e", "1231", str(file)]
-        event_cmd = ["bevent", "-f", "0-96", "-y", str(file_year), "-s", "0101", "-e", "1231", str(file)]
-        game_proc = subprocess.run(game_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        event_proc = subprocess.run(event_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        game_cmd = [
+            "bgame",
+            "-f",
+            "0-85",
+            "-y",
+            str(file_year),
+            "-s",
+            "0101",
+            "-e",
+            "1231",
+            str(file),
+        ]
+        event_cmd = [
+            "bevent",
+            "-f",
+            "0-96",
+            "-y",
+            str(file_year),
+            "-s",
+            "0101",
+            "-e",
+            "1231",
+            str(file),
+        ]
+        game_proc = subprocess.run(
+            game_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        event_proc = subprocess.run(
+            event_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         with open(game_file_dest, "wb") as f:
             f.write(game_proc.stdout)
         with open(event_file_dest, "wb") as f:
             f.write(event_proc.stdout)
-        return game_proc, event_proc
-    
-    def _process_allas(self) -> None:
+        with open(self.game_log_file, "ab") as f:
+            f.write(game_proc.stderr)
+        with open(self.event_log_file, "ab") as f:
+            f.write(event_proc.stderr)
+        return None
+
+    def _process_retro_event_files(self) -> None:
+        _ = self._mkdir(self.run_dir)
         os.chdir(self.run_dir)
+        print("|| MSG @ {} || PROCESSING RETROSHEET 'allas' DATA".format(dt.now()))
         for i in self.allas_dir.iterdir():
             dest_file = self.run_dir / i.name
             shutil.copy(i, dest_file)
@@ -92,8 +130,35 @@ class RetrosheetEtl:
                 _ = self._proc_game_event(i)
         os.chdir(self.data_dir)
         _ = self._rmdir(self.run_dir)
+        _ = self._mkdir(self.run_dir)
+        _ = os.chdir(self.run_dir)
+        print(
+            "|| MSG @ {} || PROCESSING RETROSHEET 'allpost', 'events', 'ngl-events' DATA".format(
+                dt.now()
+            )
+        )
+        for i in self.allpost_dir.iterdir():
+            dest_file = self.run_dir / i.name
+            shutil.copy(i, dest_file)
+        for i in self.events_dir.iterdir():
+            dest_file = self.run_dir / i.name
+            shutil.copy(i, dest_file)
+        for i in self.ngldata_events_dir.iterdir():
+            dest_file = self.run_dir / i.name
+            shutil.copy(i, dest_file)
+        for i in self.rosters_dir.iterdir():
+            dest_file = self.run_dir / i.name
+            shutil.copy(i, dest_file)
+        for i in self.teams_dir.iterdir():
+            dest_file = self.run_dir / i.name
+            shutil.copy(i, dest_file)
+        for i in self.run_dir.glob(r"**/*"):
+            if i.suffix in self.file_extensions:
+                _ = self._proc_game_event(i)
+        os.chdir(self.data_dir)
+        _ = self._rmdir(self.run_dir)
         return None
-    
+
     def _rmdir(self, path: Path) -> None:
         if path.exists():
             shutil.rmtree(path)
@@ -120,8 +185,11 @@ class RetrosheetEtl:
         _ = self._mkdir(self.output_dir)
         _ = self._mkdir(self.game_output_dir)
         _ = self._mkdir(self.event_output_dir)
-        _ = self._mkdir(self.run_dir)
-        print("|| MSG @ {} || DOWNLOADING AND EXTRACTING SOURE DATA FROM https://www.retrosheet.org/".format(dt.now()))
+        print(
+            "|| MSG @ {} || DOWNLOADING AND EXTRACTING SOURE DATA FROM https://www.retrosheet.org/".format(
+                dt.now()
+            )
+        )
         _ = self._download_source_data()
         _ = self._unzip(self.all_data_zip, self.extract_dir)
         for i in self.extract_dir.glob("*.zip"):
@@ -130,8 +198,7 @@ class RetrosheetEtl:
         for i in self.ngldata_dir.glob("*.zip"):
             extract_dir = self.ngldata_dir / i.name.replace(".zip", "")
             _ = _retl._unzip(i, extract_dir, remove=True)
-        print("|| MSG @ {} || PROCESSING 'allas' DATA".format(dt.now()))
-        _ = self._process_allas()
+        _ = self._process_retro_event_files()
         end = time.time()
         run_time = round((end - start) / 60, 1)
         print(
@@ -144,3 +211,33 @@ class RetrosheetEtl:
 if __name__ == "__main__":
     _retl = RetrosheetEtl()
     _ = _retl.execute()
+
+
+
+
+
+game_cols_file = Path(r"./data/metadata/game_fields.csv")
+event_cols_file = Path(r"./data/metadata/event_fields.csv")
+game_output_dir = Path(r"C:\Data\Retrosheet\output\game")
+event_output_dir = Path(r"C:\Data\Retrosheet\output\event")
+game_cols_data = pd.read_csv(game_cols_file)
+event_cols_data = pd.read_csv(event_cols_file)
+game_cols = []
+for i in game_cols_data["ColumnName"]:
+    game_cols.append(i)
+event_cols = []
+for i in event_cols_data["ColumnName"]:
+    event_cols.append(i)
+dfs_game = []
+for i in game_output_dir.iterdir():
+    df = pd.read_csv(i, names=game_cols)
+    df["SourceFile"] = str(i)
+    dfs_game.append(df)
+df_game = pd.concat(dfs_game)
+dfs_event = []
+for i in event_output_dir.iterdir():
+    df = pd.read_csv(i, names=event_cols)
+    df["SourceFile"] = str(i)
+    dfs_event.append(df)
+df_event = pd.concat(dfs_event,)
+len(df_event)
