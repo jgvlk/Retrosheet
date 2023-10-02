d069bc527f2ed15570d6954fc38f0f97c2971e0c
Jonathan Vlk <jonathan.vlk10@gmail.com>
2023-10-02 14:34:45 -0500

    cleanup

--- db/session_manager.py
+++ db/session_manager.py
@@ -4,14 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 
-db_conn_str_raw_retro = (
-    "DRIVER={SQL Server};SERVER=.\MSSQLSERVER01;DATABASE=Retrosheet;"
+db_conn_str_raw = (
+    "DRIVER={SQL Server};SERVER=.;DATABASE=Retrosheet;"
 )
-db_conn_str_retro = quote_plus(db_conn_str_raw_retro)
-db_conn_str_retro = f"mssql+pyodbc:///?odbc_connect={db_conn_str_retro}"
-engine_retro = create_engine(db_conn_str_retro)
+db_conn_str = f"mssql+pyodbc:///?odbc_connect={quote_plus(db_conn_str_raw)}"
+engine = create_engine(db_conn_str)
 
 
 class SessionManager(object):
     def __init__(self):
-        self.session = Session(engine_retro)
+        self.session = Session(engine)
